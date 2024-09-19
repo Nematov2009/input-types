f37e1e376e06ed1203ab62e1b03650f2328016f9
Nematov2009 <nematovsamandar2009@gmail.com>
2024-09-19 10:48:19 +0500

    input type

---
+++ scrib.js
@@ -0,0 +1,81 @@
+// // let  a = prompt("a=")
+// let b = prompt("b=")
+
+// let S=a*b
+// console.log("yuza",S);
+// let P=2*(a+b)
+
+// console.log("perimetr",P)
+
+// let a = prompt("a=")
+
+
+// let V=a**3
+
+// let S=6*a**2
+
+// console.log("hajmi",V)
+// console.log("sirti",S)
+
+
+// let a = prompt("a=")
+// let b = prompt("b=")
+
+// let S=a*b
+// let p=2*(a+b)
+
+// console.log("yuzasi",S);
+// console.log("perimetr",p)
+
+// let begin2 
+// let d = prompt('d=')
+// let L=3.14*d
+
+// console.log("diametr",L);
+
+// let begin4
+// let a = prompt("a=")
+// let b = prompt("b=")
+// let c = prompt("c=")
+// let V=a*b*c
+// let S=2*(a*b+b*c+a*c)
+
+// console.log("paralapiped",V);
+// console.log("hajmi",S)
+
+// let                 begin5
+// let a = prompt("a=")
+// let b = prompt("b=")
+
+// let M=(a+b)/2
+
+// console.log("o'rta arifmetig",M);
+
+// 6-misol
+// let a = prompt( "a=" )
+// let b = prompt( "b=" )
+// let c=( a ** 2 + b ** 2 ) ** ( 1/2 )
+// let P= (+a) + (+b) + c
+// console.log( "gipotenuza" ,c )
+// console.log( "perimetr" ,P )
+
+// console.log( 1 + '1' );
+
+
+//   console.log(  64 ** ( 1/2 )  );
+//   7-misol
+
+// let r1 = prompt("r1=")
+// let r2 = prompt("r2=")
+// let S1 = 3.14 * r1
+// let S2 = 3.14 * r2
+// let s = S1-S2
+// console.log("S1-S2", s);
+
+                        // 7-misol
+// let L = prompt("l=")
+// let R = L / (2 * 3.14) 
+// let S = 3.14* R **2   
+
+// console.log("radiusi",R)
+// console.log("yuzasi",S)
